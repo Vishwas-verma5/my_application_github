10c647906a30df8d57bd390ad276ea91daf223f2
Pushpendra <azadsharma343@gmail.com>
2023-06-12 15:46:23 +0530

    This is new

--- app/src/main/java/com/example/my_application_github/MainActivity.java
+++ app/src/main/java/com/example/my_application_github/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.my_application_github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv=findViewById(R.id.tv);
+        tv.setText("This is a Pushpendra");
     }
 }
