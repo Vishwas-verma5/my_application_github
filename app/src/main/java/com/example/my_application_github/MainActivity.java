dbaa19bac350ab530e8d2f80d26572ebdbaba36a
Transport <vermavishwas2111479@gmail.com>
2023-06-12 15:15:41 +0530

    hjgjhj

--- app/src/main/java/com/example/my_application_github/MainActivity.java
+++ app/src/main/java/com/example/my_application_github/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.my_application_github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+TextView ted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ted=findViewById(R.id.tv);
+        ted.setText("this is a good one");
+
+
     }
 }
